a4f6d302d5cf1ba5a12b1fdf04c7b3a6c9196a2f
Adriano <driano.carnaroli@zarpsystem.com.br>
2019-08-09 22:09:10 -0300

    Initial commit

---
+++ app/src/main/java/com/adrianocarnaroli/helloworld/TerceiraActivity.java
@@ -0,0 +1,16 @@
+package com.adrianocarnaroli.helloworld;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TerceiraActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terceira);
+    }
+
+}
